b6dfbce08a8497a5a8453e0c9d8a44103f32f1a3
Hung Nguyen <hungnb94@gmail.com>
2024-10-14 17:28:20 +0700

    Update sample app to load transcript from input video link/id

--- app/src/main/java/com/leoh/youtubetranscript/MainActivity.kt
+++ app/src/main/java/com/leoh/youtubetranscript/MainActivity.kt
@@ -3,6 +3,7 @@ package com.leoh.youtubetranscript
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,8 +15,10 @@ import io.github.thoroldvix.api.YoutubeTranscriptApi
 import io.github.thoroldvix.internal.TranscriptApiFactory
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var edtYoutubeLink: EditText
     private lateinit var btnGetTranscript: Button
     private lateinit var tvContent: TextView
     private lateinit var loadingView: View
@@ -31,6 +34,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        edtYoutubeLink = findViewById(R.id.edtLink)
+        edtYoutubeLink.setText("https://www.youtube.com/watch?v=hBtxSzKhM44")
         btnGetTranscript = findViewById(R.id.btnGetTranscript)
         tvContent = findViewById(R.id.tvContent)
         loadingView = findViewById(R.id.loadingView)
@@ -49,7 +54,7 @@ class MainActivity : AppCompatActivity() {
 
         val youtubeTranscriptApi: YoutubeTranscriptApi =
             TranscriptApiFactory.createWithClient(youtubeClient)
-        val transcriptList = youtubeTranscriptApi.listTranscripts("jw_CWHs2YDU").toList()
+        val transcriptList = youtubeTranscriptApi.listTranscripts(getVideoId()).toList()
         if (transcriptList.isNotEmpty()) {
             val transcript = transcriptList.first()
             tvContent.post {
@@ -62,6 +67,17 @@ class MainActivity : AppCompatActivity() {
         }
         tvContent.post {
             loadingView.visibility = View.GONE
+        }
+    }
+
+    private fun getVideoId(): String {
+        val pattern = "(?<=youtu.be/|watch\\?v=|/videos/|embed\\/)[^#\\&\\?]*"
+        val compiledPattern = Pattern.compile(pattern)
+        val matcher = compiledPattern.matcher(edtYoutubeLink.text)
+        return if (matcher.find()) {
+            matcher.group()
+        } else {
+            return edtYoutubeLink.text.toString()
         }
     }
 }
